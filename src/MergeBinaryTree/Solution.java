3fa21cb07707cdb875af51b263c649acb63f58e3
victorcheng <2269192760@qq.com>
2019-07-20 15:48:58 +0800

    Merge two Binary tree

--- src/MergeBinaryTree/Solution.java
+++ src/MergeBinaryTree/Solution.java
@@ -6,4 +6,24 @@ package MergeBinaryTree;
  * @Description: https://leetcode.com/problems/merge-two-binary-trees/
  */
 public class Solution {
+  public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+    return mergeNode(t1, t2);
+  }
+
+  // 递归 merge 两个树节点
+  public static TreeNode mergeNode(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) {
+      return null;
+    }
+    if (t1 == null) {
+      return t2;
+    }
+    if (t2 == null) {
+      return t1;
+    }
+    TreeNode newNode = new TreeNode(t1.val + t2.val);
+    newNode.left = mergeNode(t1.left,t2.left);
+    newNode.right = mergeNode(t1.right,t2.right);
+    return newNode;
+  }
 }
