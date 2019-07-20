94efd1b13ed97f1b1e8177704fb0de7a4f906889
victorcheng <2269192760@qq.com>
2019-07-20 17:37:33 +0800

    merge two binary tree, add test example

--- src/MergeBinaryTree/TestExample.java
+++ src/MergeBinaryTree/TestExample.java
@@ -6,4 +6,37 @@ package MergeBinaryTree;
  * @Description:
  */
 public class TestExample {
+  public static void main(String[] args) {
+    TreeNode node1 = new TreeNode(1);
+    node1.left = new TreeNode(3);
+    node1.left.left = new TreeNode(5);
+    node1.right = new TreeNode(2);
+    recursionPreOrderTraversal(node1);
+
+    System.out.println("");
+
+    TreeNode node2 = new TreeNode(2);
+    node2.left = new TreeNode(1);
+    node2.left.right = new TreeNode(4);
+    node2.right = new TreeNode(3);
+    node2.right.right = new TreeNode(7);
+
+    recursionPreOrderTraversal(node2);
+
+    System.out.println("");
+
+    Solution solution = new Solution();
+    TreeNode mergedNode = solution.mergeTrees(node1, node2);
+    recursionPreOrderTraversal(mergedNode);
+
+  }
+
+  // 深度优先 ，先遍历
+  private static void recursionPreOrderTraversal(TreeNode treeNode) {
+    if (treeNode != null) {
+      System.out.print(treeNode.val + " ");
+      recursionPreOrderTraversal(treeNode.left);
+      recursionPreOrderTraversal(treeNode.right);
+    }
+  }
 }
