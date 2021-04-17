879cf3decb1c951d89208b9d6461386e6d0c866b
vitctor <2269192760@qq.com>
2021-04-17 13:06:57 +0800

    update

--- src/design_pattern/dynamic_proxy/Hello.java
+++
@@ -1,5 +0,0 @@
-package design_pattern.dynamic_proxy;
-
-interface Hello {
-    void sayHello();
-}
--- src/design_pattern/dynamic_proxy/HelloImpl.java
+++
@@ -1,8 +0,0 @@
-package design_pattern.dynamic_proxy;
-
-class HelloImpl implements Hello {
-    @Override
-    public void sayHello() {
-        System.out.println("Hello word");
-    }
-}
